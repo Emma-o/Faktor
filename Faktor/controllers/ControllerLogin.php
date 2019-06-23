4eecc0b38526496a3a824c3f7f64ddb092385e4d
Emma-o <37258888+Emma-o@users.noreply.github.com>
2019-06-23 01:54:06 -0500

    Login
    Hecho hasta login sin conexión a db.

--- Faktor/controllers/ControllerLogin.php
+++ Faktor/controllers/ControllerLogin.php
@@ -2,21 +2,19 @@
 include_once("models/ModelLogin.php");
 class Controller {
 public $model;
-public function __construct()
-    {
+
+public function __construct(){
         $this->model = new Model();
     }
-public function invoke()
-{
-$reslt = $this->model->getlogin();     // it call the getlogin() function of model class and store the return value of this function into the reslt variable.
+
+public function invoke(){
+$reslt = $this->model->getlogin();   
 if($reslt == 'login')
-{
-include 'views/Afterlogin.php';
-}
-else
-{
-include 'views/login.php';
-}
-}
-}
+	{
+	include 'views/Afterlogin.php';
+	}else{
+	include 'views/login.php';
+	}
+   }
+ }
 ?>
